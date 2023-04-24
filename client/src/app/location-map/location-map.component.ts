59c415234b21155cc2130e5542bd5111eea18ae5
gdgdgdrox <gdfoo94@gmail.com>
2023-04-24 16:33:45 +0800

    initial commit of a copy

--- client/src/app/location-map/location-map.component.ts
+++ client/src/app/location-map/location-map.component.ts
@@ -15,14 +15,16 @@ export class LocationMapComponent implements AfterViewInit{
   constructor(private elementRef: ElementRef){}
 
   ngAfterViewInit(): void {
-    const map = new google.maps.Map(this.elementRef.nativeElement.querySelector('#mapContainer'), {
-      center: { lat: this.latitude, lng: this.longitude },
-      zoom: 15,
-    });
-  
-    const marker = new google.maps.Marker({
-      position: { lat: this.latitude, lng: this.longitude },
-      map,
-    });
+    if (this.latitude !== 0 && this.longitude !==0){
+      const map = new google.maps.Map(this.elementRef.nativeElement.querySelector('#mapContainer'), {
+        center: { lat: this.latitude, lng: this.longitude },
+        zoom: 15,
+      });
+    
+      const marker = new google.maps.Marker({
+        position: { lat: this.latitude, lng: this.longitude },
+        map,
+      });
+    }
   }
 }
